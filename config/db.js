68afc6ac63e4be9f3c26ca03ca2dbc42d272e64c
carlo-baron <carlobaron430@gmail.com>
2025-11-11 19:43:47 +0800

    refactor: removed the return conn

--- config/db.js
+++ config/db.js
@@ -4,9 +4,8 @@ dotenv.config();
 
 export default async function connectDB() {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('DB Connection Successful');
-    return conn;
   } catch (error) {
     console.error('DB Connection Unsuccessful:', error.message);
     process.exit(1);
